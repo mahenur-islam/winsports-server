cd8ef591d27bced1342fb3975c715797e15d4ba3
Mahenur Islam <mahenurislam97@gmail.com>
2023-11-13 19:04:33 +0600

    wishlist collection  and required api endpoint created

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-//middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const uri = "mongodb+srv://mahenurislam97:XQE43t5Iz7oFJucz@cluster0.neajhbt.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://mahenurislam97:XQE43t5Iz7oFJucz@cluster0.neajhbt.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,129 +26,130 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    //create a database with a collection
-    const blogCollection = client.db('blogDB').collection('blogs');
+    // Create collections
+    const blogCollection = client.db("blogDB").collection("blogs");
+    const wishlistCollection = client.db("blogDB").collection("wishlist");
 
-    //post single data endpoint
-    app.post('/blogs', async(req, res) => {
-        const blog = req.body;
-        console.log(blog)
-        const result = await blogCollection.insertOne(blog);
-        // console.log(result);
-        res.send(result);
-    })
+    // Endpoints for blogs
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      const result = await blogCollection.insertOne(blog);
+      res.send(result);
+    });
 
-    //get all blogs data endpoint
-  
-    app.get('/blogs', async(req, res) => {
-        const result = await blogCollection.find().toArray();
-        // console.log(result);
-        res.send(result);
-    })
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollection.find().toArray();
+      res.send(result);
+    });
 
-    //delete a blog 
-    app.delete("/blogs/:id", async(req, res)=>{
-        const id = req.params.id;
-        console.log('delete', id);
-        //query for delete using id field
-        const query = {
-            _id: new ObjectId(id),
+    app.delete("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await blogCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await blogCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {
+        _id: new ObjectId(id),
+      };
+      const data = req.body;
+      const options = { upsert: true };
+      const updatedData = {
+        $set: {
+          title: data.title,
+          shortDescription: data.shortDescription,
+          content: data.content,
+          category: data.category,
+          images: data.images,
+          imagePrevieUrls: data.imagePrevieUrls,
+        },
+      };
+      const result = await blogCollection.updateOne(
+        filter,
+        updatedData,
+        options
+      );
+      res.send(result);
+    });
+
+    // Get all wishlist items
+    app.get("/wishlist", async (req, res) => {
+      try {
+        const result = await wishlistCollection.find().toArray();
+        res.json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });h
+
+    // Endpoint to add a blog to the wishlist
+    app.post("/wishlist/:id", async (req, res) => {
+      const blogId = req.params.id;
+
+      try {
+        // Find the blog by ID from the blogs collection
+        const blog = await blogCollection.findOne({
+          _id: new ObjectId(blogId),
+        });
+
+        if (!blog) {
+          return res.status(404).json({ error: "Blog not found" });
         }
-        const result= await blogCollection.deleteOne(query)
-        res.send(result);
-    })
 
-    //get single data using id
-    app.get("/blogs/:id", async(req, res)=>{
-        const id = req.params.id;
-        console.log('id', id);
-        //query for delete using id field
-        const query = {
-            _id: new ObjectId(id),
-        }
-        const result= await blogCollection.findOne(query)
-        res.send(result);
-    })
+        // Add the blog to the wishlist collection
+        const result = await wishlistCollection.insertOne(blog);
+        res.json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
+    // Additional endpoints...
 
-    //update single blog 
-    app.put('/blogs/:id', async(req, res) => {
-        const id = req.params.id;
-        const filter = {
-            _id: new ObjectId(id),
-        }
-        const data = req.body;
-        const options = {upert:true};
-        const updatedData ={
-            $set:{
-                title: data.title,
-                shortDescription: data.shortDescription,
-                content: data.content,
-                category: data.category,
-                images: data.images,
-                imagePrevieUrls: data.imagePrevieUrls,
-            }
-        }
-        const result = await blogCollection.updateOne(filter,updatedData,options)
-        res.send(result);
+    app.get("/", (req, res) => {
+      res.send("Winsports server is running");
+    });
 
-    })
-    const category = [
-        {
-          category_id: 1,
-          category_title: "Cricket",
-        },
-        {
-          category_id: 2,
-          category_title: "Soccer",
-        },
-        {
-          category_id: 3,
-          category_title: "Tennis",
-        },
-        {
-          category_id: 4,
-          category_title: "Boxing",
-        },
-        {
-          category_id: 5,
-          category_title: "Car Race",
-        },
-        {
-          category_id: 6,
-          category_title: "Swimming",
-        },
+    app.get("/category", (req, res) => {
+      const category = [
+        { category_id: 1, category_title: "Cricket" },
+        { category_id: 2, category_title: "Soccer" },
+        { category_id: 3, category_title: "Tennis" },
+        { category_id: 4, category_title: "Boxing" },
+        { category_id: 5, category_title: "Car Race" },
+        { category_id: 6, category_title: "Swimming" },
       ];
-      
-      app.get("/", (req, res) => {
-        res.send("winsports server is running");
-      });
-      
-      
-      
-      //make an api
-      app.get('/category', (req, res)=>{
-          res.send(category);
-      })
-      
-      
-      
-      
-      app.listen(port, () => {
-        console.log(`server is running on port: ${port}`);
-      });
-      
+      res.send(category);
+    });
 
-
-
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
 
+run().catch(console.dir);
