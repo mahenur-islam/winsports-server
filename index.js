fe163c46f6435e6a847c47f1205cd5e77cf38d5c
Mahenur Islam <mahenurislam97@gmail.com>
2023-12-04 13:20:46 +0600

    server is working properly, some changes are made for commentCollection

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     // Create collections
     const blogCollection = client.db("blogDB").collection("blogs");
     const wishlistCollection = client.db("blogDB").collection("wishlist");
+    const commentsCollection = client.db("blogDB").collection("comments");
 
     // Endpoints for blogs
     app.post("/blogs", async (req, res) => {
@@ -73,15 +74,10 @@ async function run() {
           shortDescription: data.shortDescription,
           content: data.content,
           category: data.category,
-          images: data.images,
-          imagePrevieUrls: data.imagePrevieUrls,
+          image: data.image,
         },
       };
-      const result = await blogCollection.updateOne(
-        filter,
-        updatedData,
-        options
-      );
+      const result = await blogCollection.updateOne(filter, updatedData, options);
       res.send(result);
     });
 
@@ -118,42 +114,88 @@ async function run() {
         res.status(500).json({ error: "Internal Server Error" });
       }
     });
-    //endpoints for deleting a blog from wishlist
-    app.delete("/wishlist/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = {
-          _id: new ObjectId(id),
-        };
-        const result = await wishlistCollection.deleteOne(query);
-        res.send(result);
-      });
-    // Additional endpoints...
 
+    // Endpoint to remove a blog from the wishlist
+app.delete("/wishlist/:id", async (req, res) => {
+  const blogId = req.params.id;
+
+  try {
+    const result = await wishlistCollection.deleteOne({
+      _id: new ObjectId(blogId),
+    });
+
+    if (result.deletedCount > 0) {
+      res.json({ success: true, message: "Blog removed from wishlist successfully" });
+    } else {
+      res.status(404).json({ error: "Blog not found in the wishlist" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
+    // Endpoints for comments
+  // Endpoint to add a comment to a blog
+  app.post("/comments/:id", async (req, res) => {
+    const blogId = req.params.id;
+    const { comment, email } = req.body;
+
+    // Fetch blog details to get authorEmail
+    const blogDetails = await blogCollection.findOne({
+      _id: new ObjectId(blogId),
+    });
+
+    if (!blogDetails) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    const { authorEmail } = blogDetails;
+
+    // Check if the user's email is equal to the author's email
+    if (user && user.email === authorEmail) {
+      return res.status(403).json({ error: "You cannot comment on your own blog" });
+    }
+
+    try {
+      const result = await commentsCollection.insertOne({
+        blogId: new ObjectId(blogId),
+        comment,
+        email,
+        timestamp: new Date(),
+      });
+
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+    // Endpoint to fetch comments for a specific blog
+    app.get("/comments/:id", async (req, res) => {
+      const blogId = req.params.id;
+
+      try {
+        const result = await commentsCollection.find({ blogId: new ObjectId(blogId) }).toArray();
+        res.json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // Additional endpoints...
+    
     app.get("/", (req, res) => {
       res.send("Winsports server is running");
     });
 
-    app.get("/category", (req, res) => {
-      const category = [
-        { category_id: 1, category_title: "Cricket" },
-        { category_id: 2, category_title: "Soccer" },
-        { category_id: 3, category_title: "Tennis" },
-        { category_id: 4, category_title: "Boxing" },
-        { category_id: 5, category_title: "Car Race" },
-        { category_id: 6, category_title: "Swimming" },
-      ];
-      res.send(category);
-    });
-
-    app.listen(port, () => {
-      console.log(`Server is running on port: ${port}`);
-    });
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -161,3 +203,8 @@ async function run() {
 }
 
 run().catch(console.dir);
+
+// This should be outside the try block, and app.listen should only be called once
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
