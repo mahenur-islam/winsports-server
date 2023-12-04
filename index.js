f2e69baa955a7e8b4adc109d2e4c904fd442395b
Mahenur Islam <mahenurislam97@gmail.com>
2023-12-04 13:55:44 +0600

    environment variable created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -10,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://mahenurislam97:XQE43t5Iz7oFJucz@cluster0.neajhbt.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.neajhbt.mongodb.net/?retryWrites=true&w=majority`;
+  console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
